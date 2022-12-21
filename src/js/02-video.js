016d6063174f85b715b2d3eb8b86f95a54bd77d5
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2022-12-21 21:43:24 +0100

    add fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,16 @@
 import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
