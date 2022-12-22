4ee421fbab87c0013205b1a37788e95e637034ae
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2022-12-23 00:23:12 +0100

    правка

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,14 @@
+import throttle from 'lodash.throttle'; 
 import Player from '@vimeo/player';
+
+const vimeo = document.getElementById("vimeo-player")
 
 const players = new Player('handstick', {
     id: 19231868,
     width: 640
 });
 
-players.on('play', function(currentTime) {
-    const seconds = currentTime.seconds
-localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds))
-});
+players.on('timeupdate', throttle(({ seconds }) =>
+    localStorage.setItem('videoplayer-current-time', seconds)), 500);
 
+console.log("привіт")
