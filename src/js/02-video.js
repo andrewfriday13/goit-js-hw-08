4966f00fb70006d6d0dd57b7d829c1a36126a158
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2022-12-23 22:44:02 +0100

    add 3 part

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,22 @@
 import throttle from 'lodash.throttle'; 
+import Player from '@vimeo/player'
 
 const iframe = document.querySelector('iframe');
 
-const players = new Vimeo.Player(iframe);
-console.log(players)
+const players = new Player(iframe)
 
-players.on('timeupdate', throttle(({ seconds }) => localStorage.setItem('videoplayer-current-time', seconds)), 500);
+players.on('timeupdate', throttle(({ seconds }) => localStorage.setItem('videoplayer-current-time', seconds)), 1100);
+
+players.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle'
+const feedBackForm = document.querySelector(".feedback-form")
+const textarea = document.querySelector("textarea")
+const input = document.querySelector("input")
+
+const allData  = {}
+const TEXTEAREA_KEY = "feedback-form-state"
+
+qwe()
+feedBackForm.addEventListener('submit', submitForm)
+textarea.addEventListener('input', throttle(textareaForm, 500))
+
+feedBackForm.addEventListener('input', evnt => {
+
+  allData[evnt.target.name] = evnt.target.value
+  console.log(allData)
+}
+)
+
+
+function submitForm(evnt) { 
+  evnt.preventDefault()
+  evnt.target.reset()
+  localStorage.removeItem(TEXTEAREA_KEY)
+}
+
+function textareaForm() {
+
+  const feedBackMessage = JSON.stringify(allData)
+ localStorage.setItem(TEXTEAREA_KEY, feedBackMessage)
+}
+
+function qwe() {
+  const saveStorage = localStorage.getItem(TEXTEAREA_KEY)
+
+  const translate = JSON.parse(saveStorage)
+  
+  if (translate) {
+    textarea.value = translate.message
+    input.value = translate.email
+  }
+}
+
 
